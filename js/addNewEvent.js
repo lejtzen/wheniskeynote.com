bb2621fb4fdd400717ae90e9d6c7d21ca2fee9a7
Vincent Lejtzén <vincent@lejtzendesign.se>
2020-09-09 08:28:54 +0200

    Update addNewEvent.js (#1)

--- js/addNewEvent.js
+++ js/addNewEvent.js
@@ -1,18 +1,18 @@
 /* --------------------------
 * UPDATE UPCOMING EVENT DATE
-* --------------------------
+* -------------------------- 
 *
 * IMPORTANT: The date has to be in the Pacific Standard Time (PST) zone
 *
 */
-const
+const 
 // Format: YYYY (2020)
 year = 2020,
-// Format: MM (09) or M (9), both are valid
-month = 06,
+// Format: MM (09) or M (9), both are valid 
+month = 09,
 // Format: DD (09) or D (9), both are valid
-day = 22
-// Format: 0 - 24 / 5 == 5 AM / 17 == 5 PM
+day = 15  
+// Format: 0 - 24 / 5 == 5 AM / 17 == 5 PM 
 hour = 10,
 // Format: MM (09) or M (9), both are valid
 minute = 00;
@@ -21,4 +21,4 @@ minute = 00;
 * UPDATE UPCOMING EVENT NAME
 * --------------------------
 */
-const eventName = 'WWDC 2020';
+const eventName = 'Apple September Event';
