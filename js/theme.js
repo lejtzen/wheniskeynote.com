4462622310729e05c9352425f65f3181a14bafa9
Vincent Lejtzén <vincent@lejtzendesign.se>
2020-11-07 20:55:20 +0100

    Add prettier config

--- js/theme.js
+++ js/theme.js
@@ -1,24 +1,24 @@
-(function(el) {
+;(function (el) {
   window.theme = {
     colors: ['light', 'dark'],
 
     default: 'dark',
 
-    set color (color) {
-      var theme = this.colors.indexOf(color) > -1 ? color : this.default;
+    set color(color) {
+      var theme = this.colors.indexOf(color) > -1 ? color : this.default
 
-      el.dataset.theme = theme;
-      window.localStorage.setItem('theme', theme);
+      el.dataset.theme = theme
+      window.localStorage.setItem('theme', theme)
     },
 
-    get color () {
-      return el.dataset.theme;
+    get color() {
+      return el.dataset.theme
     },
 
     toggle: function () {
-      this.color = this.color === 'dark' ? 'light' : 'dark';
+      this.color = this.color === 'dark' ? 'light' : 'dark'
     },
-  };
+  }
 
-  theme.color = window.localStorage.getItem('theme');
-})(document.documentElement);
+  theme.color = window.localStorage.getItem('theme')
+})(document.documentElement)
