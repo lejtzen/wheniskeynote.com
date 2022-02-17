79ad23070064b1fba33e4a90b47fff89fcd38eb1
Vincent Lejtzén <vincent@lejtzendesign.se>
2022-02-17 22:02:27 +0100

    Working it

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,30 @@ module.exports = {
         center: true,
         padding: '2rem',
       },
+      keyframes: {
+        fade: {
+          from: {
+            opacity: '0',
+            transform: 'translateY(50px)',
+          },
+          to: {
+            opacity: '1',
+            transform: 'translateY(0%)',
+          },
+        },
+        slide: {
+          from: {
+            transform: 'translateY(100%)',
+          },
+          to: {
+            transform: 'translateY(0%)',
+          },
+        },
+      },
+      animation: {
+        fade: 'fade 1000ms ease-out 1000ms backwards',
+        slide: 'slide 400ms ease-out',
+      },
     },
   },
   plugins: [],
