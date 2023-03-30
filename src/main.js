5304a9dc312e005e5c3e5d82ac46ec9d16c8b744
Vincent Lejtzén <vincent@lejtzendesign.se>
2023-03-30 08:49:05 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -33,12 +33,12 @@
 const // The time zone when the event will be held. Format: time zone identifier (e.g., "PDT" or "PST")
   timeZone = 'PDT',
   // Format: YYYY (2020)
-  year = 2022,
+  year = 2023,
   // The month as a number, not the index
   // Format: MM (09) or M (9), both are valid
-  month = 09,
+  month = 06,
   // Format: DD (09) or D (9), both are valid
-  day = 07,
+  day = 05,
   // Format: 0 - 24 / 5 == 5 AM / 17 == 5 PM
   hour = 10,
   // Format: MM (09) or M (9), both are valid
@@ -48,7 +48,7 @@ const // The time zone when the event will be held. Format: time zone identifier
  * UPDATE UPCOMING EVENT NAME
  * --------------------------
  */
-const eventName = 'Far Out'
+const eventName = 'WWDC23 keynote'
 
 let start = new Date(
     month + '/' + day + '/' + year + ' ' + hour + ':' + minute + ' ' + timeZone,
