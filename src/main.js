9f8b8b8dd85091c1e82b27ec0de51f0a69373322
Vincent Lejtzén <vincent@lejtzendesign.se>
2023-08-30 14:20:06 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -30,19 +30,19 @@
  * IMPORTANT: The date has to be in the Pacific Standard Time (PST) zone
  *
  */
-const // The time zone when the event will be held. Format: time zone identifier (e.g., "PDT" or "PST")
-  timeZone = 'PDT',
-  // Format: YYYY (2020)
-  year = 2023,
-  // The month as a number, not the index
-  // Format: MM (09) or M (9), both are valid
-  month = 09,
-  // Format: DD (09) or D (9), both are valid
-  day = 12,
-  // Format: 0 - 24 / 5 == 5 AM / 17 == 5 PM
-  hour = 10,
-  // Format: MM (09) or M (9), both are valid
-  minute = 00
+// The time zone when the event will be held. Format: time zone identifier (e.g., "PDT" or "PST")
+const timeZone = 'PDT'
+// Format: YYYY (2020)
+const year = '2023'
+// The month as a number, not the index
+// Format: MM (09) or M (9), both are valid
+const month = '09'
+// Format: DD (09) or D (9), both are valid
+const day = '12'
+// Format: 0 - 24 / 5 == 5 AM / 17 == 5 PM
+const hour = '10'
+// Format: MM (09) or M (9), both are valid
+const minute = '00'
 
 /* --------------------------
  * UPDATE UPCOMING EVENT NAME
