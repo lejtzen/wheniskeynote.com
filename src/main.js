4dcf453206d00a3477efe11b0716bff55141521b
Vincent Lejtzén <vincent@lejtzendesign.se>
2024-04-19 08:23:48 +0200

    Add WWDC24

--- src/main.js
+++ src/main.js
@@ -33,12 +33,12 @@
 // The time zone when the event will be held. Format: time zone identifier (e.g., "PDT" or "PST")
 const timeZone = 'PDT'
 // Format: YYYY (2020)
-const year = '2023'
+const year = '2024'
 // The month as a number, not the index
 // Format: MM (09) or M (9), both are valid
-const month = '09'
+const month = '06'
 // Format: DD (09) or D (9), both are valid
-const day = '12'
+const day = '10'
 // Format: 0 - 24 / 5 == 5 AM / 17 == 5 PM
 const hour = '10'
 // Format: MM (09) or M (9), both are valid
@@ -48,7 +48,7 @@ const minute = '00'
  * UPDATE UPCOMING EVENT NAME
  * --------------------------
  */
-const eventName = 'Apple Event'
+const eventName = 'Apple WWDC24';
 
 let start = new Date(
     month + '/' + day + '/' + year + ' ' + hour + ':' + minute + ' ' + timeZone,
