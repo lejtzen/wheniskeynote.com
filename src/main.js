2f31fcfaccd892e1396a4676d89f0b29bd04406c
Vincent Lejtzén <vincent@lejtzendesign.se>
2026-03-25 14:43:12 +0100

    Add WWDC26

--- src/main.js
+++ src/main.js
@@ -36,9 +36,9 @@ const timeZone = 'EST'
 const year = '2026'
 // The month as a number, not the index
 // Format: MM (09) or M (9), both are valid
-const month = '03'
+const month = '06'
 // Format: DD (09) or D (9), both are valid
-const day = '04'
+const day = '08'
 // Format: 0 - 24 / 5 == 5 AM / 17 == 5 PM
 const hour = '09'
 // Format: MM (09) or M (9), both are valid
@@ -48,7 +48,7 @@ const minute = '00'
  * UPDATE UPCOMING EVENT NAME
  * --------------------------
  */
-const eventName = "You're invited."
+const eventName = 'WWDC26'
 
 let start = new Date(
     month + '/' + day + '/' + year + ' ' + hour + ':' + minute + ' ' + timeZone,
